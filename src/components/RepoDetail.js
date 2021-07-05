ec805b2471166f4487b7fe423ca2e67a40e53307
Duc.MinhDang@coxautoinc.com <Duc.MinhDang@coxautoinc.com>
2021-07-05 10:14:43 +0700

    home assesment

---
+++ src/components/RepoDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RepoDetail extends Component {
+  render() {
+        return (
+            <div>The RepoDetail!!!</div>
+        )
+    }
+}
+
+export default RepoDetail;
